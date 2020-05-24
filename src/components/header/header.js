59b264425a32a035997d9b77d9c7822616edaf3e
kessris <kessris@hotmail.com>
2020-05-23 17:29:00 -0700

    initial set-up with create-react-app and redux

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div style={{backgroundColor: 'green'}}>
+            This is the header!
+        </div>
+    );
+};
+
+export default Header;
