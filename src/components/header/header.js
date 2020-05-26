4b799ea0c5f3f5a46d7fdab6095af5ca45538ccf
ChaoyuXiang <chaoyuxiang07@gmail.com>
2020-05-25 17:08:27 -0700

    first try

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+} from '@material-ui/core';
+
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from '@material-ui/icons';
 
 const Header = () => {
     return (
-        <div style={{backgroundColor: 'green'}}>
-            This is the header!
-        </div>
+        <Box componnet='header' display = "flex">
+            <AppBar position="static" >
+                <Toolbar>
+                    <IconButton>
+                        <ArrowBack style = {{color:"white"}}/>
+                    </IconButton>
+                    <IconButton>
+                    <Typography style = {{color:"white"}} >Home</Typography>
+                    </IconButton>
+                    <IconButton>
+                    <Typography style = {{color:"white"}} >Symptom Checker</Typography>
+                    </IconButton>
+                    <IconButton>
+                    <Typography style = {{color:"white"}} >Have I been Exposed?</Typography>
+                    </IconButton>
+                    <IconButton>
+                    <Typography style = {{color:"white"}} >Financial Help</Typography>
+                    </IconButton>
+                    <IconButton>
+                    <Typography style = {{color:"white"}} >Reopening Date</Typography>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
