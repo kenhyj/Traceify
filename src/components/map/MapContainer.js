fb3e493d46dae30c02483e27a2bf07642a4ab36a
Mandy Wan <mandygwan@gmail.com>
2020-06-04 19:44:18 -0700

    changed favicon

---
+++ src/components/map/MapContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MapContainer extends Component {
+    render() {
+        return(
+            <div>
+                --- map component here ---
+            </div>
+        )};
+}
+
+export default MapContainer;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import MapContainer from '../../components/map/MapContainer';
 
 const Home = () => {
     return (
         <div>
             This should be the home page.
+            <div>
+                <MapContainer />
+            </div>
         </div>
     );
 };
