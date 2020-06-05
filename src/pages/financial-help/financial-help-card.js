2c1c9e6f5d633a1a30554ae920f622cb5b0006d0
Mandy Wan <mandygwan@gmail.com>
2020-06-04 17:44:43 -0700

    created financial help cards and list

---
+++ src/pages/financial-help/financial-help-card.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './financial-help.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardContent, Typography, CardActions, Button } from '@material-ui/core';
+
+const cardStyles = makeStyles({
+    root: {
+        maxWidth: 700, // TODO: make responsive
+        variant: "outlined",
+    },
+    content: {
+        padding: 24,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 700,
+    },
+});
+
+const FinancialHelpCard = (props) => {
+    const classes = cardStyles();
+
+    const { title, description, id, url } = props;
+
+    const handleClick = () => {
+        const { url } = props;
+        window.open(url);
+    };
+
+    return (
+        <Card className={classes.root} id={id}>
+            <CardContent>
+                <Typography className={classes.title}>
+                    {title}
+                </Typography>
+                <Typography>
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="large" onClick={handleClick}>
+                    Find out more
+                </Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default FinancialHelpCard;
--- src/pages/financial-help/financial-help.js
+++ src/pages/financial-help/financial-help.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import FinancialHelpCard from './financial-help-card';
+
+// TODO: are we going to have a separate component for component headers?
 
 const FinancialHelp = () => {
+    const helpResources = [
+        {
+            title: "Canadian Emergency Response Benefit",
+            description: "The benefit provided by the Canadian Federal Government for those affected by COVID-19.",
+            url: "https://www.canada.ca/en/revenue-agency/services/benefits/apply-for-cerb-with-cra.html",
+            id: 0
+        },
+    ];
+
     return (
         <div>
-            This should be the Financial Help page.
+            <div>
+                <h2>Financial Help Resources</h2>
+            </div>
+            <div>
+            {helpResources.map(resource => 
+                <FinancialHelpCard key={resource.id} {...resource} />
+            )}
+            </div>
         </div>
     );
 };
