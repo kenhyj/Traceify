c704209e8a7cfb0c1e4f376526b57d5b32e3a7c6
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2020-06-06 14:11:52 -0700

    adjusted combine reducers appropriately

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,5 +3,5 @@ import diagnosisReducer from './reducers/diagnosis-reducer';
 
 // This is where you import all the reducers and combine it into one
 export default combineReducers({
-    symptoms: diagnosisReducer
+    diagnosis: diagnosisReducer
 });
--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import diagnosisReducer  from './diagnosis-reducer';
-
-import {combineReducers} from 'redux';
-
-const reducers = combineReducers({
-    diagnosis: diagnosisReducer,
-})
-
-export default reducers;
