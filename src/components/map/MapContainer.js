b418f586eb8f50068d1deb8371e7dcc01d99f342
Mandy <mandygwan@gmail.com>
2020-06-18 03:22:13 -0700

    separate styles .css.js file

--- src/components/map/MapContainer.js
+++ src/components/map/MapContainer.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import styles from './MapContainer.css';
 
 // NOTE: uncomment this, and comment out the actual api if you don't want to use up loads 
 const API_KEY = `${process.env.REACT_APP_GOOGLE_API_KEY}`;
 // const API_KEY = 123456;  
-
-const containerStyle = {
-    width: "800px",
-    height: "800px",
-};
-
-const center = {
-    lat: 49.2827,
-    lng: -123.1207,
-};
 
 class MapContainer extends Component {
     render() {
@@ -21,9 +12,9 @@ class MapContainer extends Component {
             <div>
                 <LoadScript googleMapsApiKey={API_KEY}>
                     <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        center={center}
-                        zoom={10}
+                        mapContainerStyle={styles.mapContainerStyle}
+                        center={styles.center}
+                        zoom={styles.zoom}
                     >
                         <></>
                     </GoogleMap>
