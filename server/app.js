9f075b380da91261c7e00c2ac12aa6252315b810
e5c0b <e5c0b@ugrad.cs.ubc.ca>
2020-07-22 16:33:20 -0700

    refresh and manually url bug fix in app.js

--- server/app.js
+++ server/app.js
@@ -1,41 +1,41 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //I think this is the Middleware
-var indexRouter = require("./routes/index");
-var adminsRouter = require("./routes/admins");
-var postsRouter = require("./routes/posts");
-var locationRouter = require("./routes/location-traces");
-var reopeningsRouter = require("./routes/reopenings");
+var indexRouter = require('./routes/index');
+var adminsRouter = require('./routes/admins');
+var postsRouter = require('./routes/posts');
+var locationRouter = require('./routes/location-traces');
+var reopeningsRouter = require('./routes/reopenings');
 var exposeRouter = require('./routes/expose');
 var locationOutbreakRouter = require('./routes/location-outbreak');
 var app = express();
-require("dotenv").config({ path: "./.env" });
+require('dotenv').config({ path: './.env' });
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use(exposeRouter);
 app.use(adminsRouter);
 app.use(locationRouter);
 app.use(locationOutbreakRouter);
-app.use("/posts", postsRouter);
-app.use("/reopenings", reopeningsRouter);
+app.use('/posts', postsRouter);
+app.use('/reopenings', reopeningsRouter);
 app.use(bodyParser.json());
 
 // CONNECT TO MONGO DB
@@ -44,15 +44,20 @@ mongoose.connect(process.env.DB_CONNECTION, {
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB is connected");
+mongoose.connection.on('connected', () => {
+  console.log('mongoDB is connected');
 });
 
-const db = require("./db");
+const db = require('./db');
 db.connect(() => {
   app.listen(process.env.PORT || 5555, function () {
     console.log(`Listening`);
   });
+});
+
+// this should prevent Not found when refreshing or manually typing url
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // catch 404 and forward to error handler
@@ -64,11 +69,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
