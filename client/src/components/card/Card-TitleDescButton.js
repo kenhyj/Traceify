507b3563cb79271e00de7c85f83f9f59773c0f86
ChaoyuXiang <chaoyuxiang07@gmail.com>
2020-07-30 23:32:27 -0700

    finished some responsive design in symptom checkers

--- client/src/components/card/Card-TitleDescButton.js
+++ client/src/components/card/Card-TitleDescButton.js
@@ -4,74 +4,73 @@ import {
   Card,
   CardContent,
   Typography,
-  CardActions,
   Button,
+  Hidden,
+  Collapse,
 } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 
-const cardStyles = makeStyles({
+const cardStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 800, // TODO: make responsive
+    maxWidth: '800px',
+    [theme.breakpoints.down('md')]: {
+      maxWidth: '300px',
+    },
     variant: 'outlined',
-    margin: 10,
+    margin: 5,
   },
   content: {
-    padding: 20,
+    padding: 5,
+
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 700,
+  contentText: {
+    marginTop: '5px',
+    fontSize: '0.8rem',
   },
-  // btn: {
-  //   fontWeight: '700',
-  //   backgroundColor: '#00a1c0',
-  //   color: '#ffffff',
-  //   display: 'inline-flex',
-  //   alignItems: 'center',
-  //   boxShadow:
-  //     'rgba(0, 0, 0, 0.24) 0px 2px 2px 0px, rgba(0, 0, 0, 0.24) 0px 0px 1px 0px',
-  //   padding: '0px',
-  //   borderRadius: '3',
-  //   border: '1px solid transparent',
-  //   '&:hover, &.Mui-focusVisible': {
-  //     backgroundColor: '#00a1c0',
-  //     opacity: '0.7',
-  //     cursor: 'pointer',
-  //     transition: '0.7s',
-  //   },
-  // },
-  // btnSpan: {
-  //   padding: '0.5rem',
-  // },
-});
+  button: {
+    color: 'inherit',
+    size: 'small'
+  },
+  alert:{
+    fontSize:'0.8rem'
+  }
+}));
 
 const CardTitleDescButton = (props) => {
   const classes = cardStyles();
 
-  const { title, descriptions, id, url } = props;
+  const { title, descriptions, id, url, severe } = props;
 
   const handleClick = () => {
     window.open(url);
   };
 
+  const handleSuggestion = ()=> {
+    if(title === 'Suggestions'){
+      return (
+        <Hidden mdDown>
+        {descriptions.map((oneDescription) => {
+          return <Typography className={classes.contentText}>{oneDescription}</Typography>;
+        })}
+        </Hidden>
+      )
+    }
+    else return (descriptions.map((oneDescription) => {
+      return <Typography className={classes.contentText}>{oneDescription}</Typography>;
+    }));
+  }
+
   return (
     <Card className={classes.root} id={id}>
       <CardContent>
-        <Typography className={classes.title}>{title}</Typography>
-        {descriptions.map((oneDescription) => {
-          return <Typography>{oneDescription}</Typography>;
-        })}
+        <Alert className={classes.alert}
+          severity={severe}
+          action={
+            <Button className={classes.button} onClick={() => handleClick(url)}>More</Button>
+          }>{title}
+        </Alert>
+        {handleSuggestion()}
       </CardContent>
-      <CardActions>
-        <Button
-          color='primary'
-          className='buttonz'
-          variant='outlined'
-          onClick={handleClick}
-          style={{width:'40%'}}
-        >
-          Find out more
-        </Button>
-      </CardActions>
     </Card>
   );
 };
