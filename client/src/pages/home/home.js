d371cbf2dba3217129824e692e0fe1253d58a732
Mandy <mandygwan@gmail.com>
2020-08-01 16:44:55 -0700

    resize based on window size

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { Hidden, Chip } from '@material-ui/core';
 import MapContainer from './map/MapContainer';
 import Sidebar from './sidebar';
 import PageHeading from '../../components/page-heading/PageHeading';
 import { variants, transitions, pageStyle } from '../motion-settings';
 import './home.css';
-import { Hidden, Chip } from '@material-ui/core';
 
 const Home = () => {
   const heading = 'Interactive Contact Tracing Map';
@@ -14,7 +14,26 @@ const Home = () => {
   const body =
     'See where individuals who have tested positive for COVID-19 have been recently by interacting with the map below.';
   const pageHeadingData = { heading, subheading, body };
-  const [mapVisible, setVisible] = useState(false);
+  const [mapVisible, setMapVisible] = useState(false);
+
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <motion.div
@@ -28,25 +47,22 @@ const Home = () => {
       <div>
         <PageHeading data={pageHeadingData} />
       </div>
-      <Hidden mdUp>
+      <div className='Home-main-body'>
         <div className='Home-mobile-toggle-view'>
-          <Chip label='View Maps' onClick = {()=>setVisible(!mapVisible)}></Chip>
-          <Chip label='View Info'></Chip>
+          <Chip
+            onClick={() => setMapVisible(!mapVisible)}
+            label='Toggle View'
+          />
         </div>
-      </Hidden>
-      <div className="Home-main-body">
-        <Sidebar className="Home-main-body-sidebar" />
-        <Hidden smDown>
-        <div style={{width: `${window.innerWidth - (window.innerWidth)*0.3}px`}}>
-          <MapContainer className="Home-main-body-map" />
+        <Sidebar className='Home-main-body-sidebar' />
+        <div
+          style={{
+            width: `${windowSize.width - windowSize.width * 0.3}px`,
+          }}
+        >
+          <MapContainer className='Home-main-body-map' />
         </div>
-        </Hidden>
       </div>
-      <Hidden mdUp>
-      <div style = {{width:'80%', margin:'10px 10%', display : mapVisible?'block':'none'}}>
-        <MapContainer className="Home-main-body-map" />
-        </div>
-      </Hidden>
     </motion.div>
   );
 };
